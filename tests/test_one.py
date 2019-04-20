49bd4944cc0605afc380ddd20a7a93f5119b3e80
Don Jayamanne <don.jayamanne@yahoo.com>
2019-04-19 17:02:17 -0700

    Update test_one.py

--- tests/test_one.py
+++ tests/test_one.py
@@ -17,30 +17,30 @@ def getData():
         return json.load(fs)
 
 class TestFirstSuite(unittest.TestCase):
-    def test_one(self):
+    def test_one_first_suite(self):
         data = getData()
         delay_running()
         self.assertEqual(1, data[0])
 
-    def test_two(self):
+    def test_two_first_suite(self):
         data = getData()
         delay_running()
         self.assertEqual(2, data[1])
 
-    def test_three(self):
+    def test_three_first_suite(self):
         data = getData()
         delay_running()
         self.assertEqual(3, data[2])
 
 class TestSecondSuite(unittest.TestCase):
-    def test_four(self):
+    def test_four_second_suite(self):
         data = getData()
         self.assertEqual(4, data[3])
 
-    def test_five(self):
+    def test_five_second_suite(self):
         data = getData()
         self.assertEqual(5, data[4])
 
-    def test_six(self):
+    def test_six_second_suite(self):
         data = getData()
         self.assertEqual(6, data[5])
